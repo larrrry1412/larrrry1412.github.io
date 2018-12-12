674fb92c28ac91b48d916f71b0cec2e5284cdac7
sketchers recognition <1244304379@qq.com>
2018-12-12 21:20:22 +0800

    Add files via upload

---
+++ recog/main.js
@@ -0,0 +1,265 @@
+/*
+variables
+*/
+var model;
+var canvas;
+var classNames = [];
+var canvas;
+var coords = [];
+var mousePressed = false;
+var mode;
+var a=[];
+
+/*
+prepare the drawing canvas 
+*/
+$(function() {
+    canvas = window._canvas = new fabric.Canvas('canvas');
+    canvas.backgroundColor = '#ffffff';
+    canvas.isDrawingMode = 0;
+    canvas.freeDrawingBrush.color = "black";
+    canvas.freeDrawingBrush.width = 10;
+    canvas.renderAll();
+    //recognize once mouse_up
+    canvas.on('mouse:up', function(e) {
+        getFrame();
+        mousePressed = false
+    });
+    canvas.on('mouse:down', function(e) {
+        mousePressed = true
+    });
+    canvas.on('mouse:move', function(e) {
+        recordCoor(e)
+    });
+})
+
+/*
+set the table of the predictions 
+*/
+function setTable(top5, probs) {
+    //loop over the predictions 
+    for (var i = 0; i < top5.length; i++) {
+        let sym = document.getElementById('sym' + (i + 1))
+        let prob = document.getElementById('prob' + (i + 1))
+        sym.innerHTML = top5[i]
+	a[i]=top5[i]
+        prob.innerHTML = Math.round(probs[i] * 100)
+    }
+    //create the pie 
+    createPie(".pieID.legend", ".pieID.pie");
+
+}
+
+/*
+record the current drawing coordinates
+*/
+function recordCoor(event) {
+    var pointer = canvas.getPointer(event.e);
+    var posX = pointer.x;
+    var posY = pointer.y;
+
+    if (posX >= 0 && posY >= 0 && mousePressed) {
+        coords.push(pointer)
+    }
+}
+
+/*
+cause the image data input should be (28,28,1)
+so get the best bounding box by trimming around the drawing
+will benefit the result
+*/
+function getMinBox() {
+    //get coordinates 
+    var coorX = coords.map(function(p) {
+        return p.x
+    });
+    var coorY = coords.map(function(p) {
+        return p.y
+    });
+
+    //find top left and bottom right corners 
+    var min_coords = {
+        x: Math.min.apply(null, coorX),
+        y: Math.min.apply(null, coorY)
+    }
+    var max_coords = {
+        x: Math.max.apply(null, coorX),
+        y: Math.max.apply(null, coorY)
+    }
+
+    //return box cordinates
+    return {
+        min: min_coords,
+        max: max_coords
+    }
+}
+
+/*
+get the image data 
+*/
+function getImageData() {
+        //get the minimum bounding box around the drawing 
+        const mbb = getMinBox()
+
+        //get image data according to dpi 
+        const dpi = window.devicePixelRatio
+        const imgData = canvas.contextContainer.getImageData(mbb.min.x * dpi, mbb.min.y * dpi,
+                                                      (mbb.max.x - mbb.min.x) * dpi, (mbb.max.y - mbb.min.y) * dpi);
+        return imgData
+    }
+
+/*
+get the prediction 
+*/
+function getFrame() {
+    //make sure we have at least two recorded coordinates 
+    if (coords.length >= 2) {
+
+        //get the image data from the canvas 
+        const imgData = getImageData()
+
+        //get the prediction 
+        const pred = model.predict(preprocess(imgData)).dataSync()
+
+        //find the top 5 predictions 
+        const indices = findIndicesOfMax(pred, 5)
+        const probs = findTopValues(pred, 5)
+        const names = getClassNames(indices)
+
+        //set the table 
+        setTable(names, probs)
+    }
+
+}
+
+/*
+get the the class names 
+*/
+function getClassNames(indices) {
+    var outp = []
+    for (var i = 0; i < indices.length; i++)
+        outp[i] = classNames[indices[i]]
+    return outp
+}
+
+/*
+load the class names 
+*/
+async function loadDict() {
+    loc = 'model2/class_names.txt'
+    
+    await $.ajax({
+        url: loc,
+        dataType: 'text',
+    }).done(success);
+}
+
+/*
+correct the class names by deleting '\n'
+*/
+function success(data) {
+    const lst = data.split(/\n/)
+    for (var i = 0; i < lst.length - 1; i++) {
+        let symbol = lst[i]
+        classNames[i] = symbol
+    }
+}
+
+/*
+get indices of the top probs
+*/
+function findIndicesOfMax(inp, count) {
+    var outp = [];
+    for (var i = 0; i < inp.length; i++) {
+        outp.push(i); // add index to output array
+        if (outp.length > count) {
+            outp.sort(function(a, b) {
+                return inp[b] - inp[a];
+            }); // descending sort the output array
+            outp.pop(); // remove the last index (index of smallest element in output array)
+        }
+    }
+    return outp;
+}
+
+/*
+find the top 5 predictions
+*/
+function findTopValues(inp, count) {
+    var outp = [];
+    let indices = findIndicesOfMax(inp, count)
+    
+    for (var i = 0; i < indices.length; i++)
+        outp[i] = inp[indices[i]]
+    return outp
+}
+
+/*
+preprocess the data
+*/
+function preprocess(imgData) {
+    return tf.tidy(() => {
+        //convert to a tensor 
+        let tensor = tf.fromPixels(imgData, numChannels = 1)
+        
+        //resize 
+        const resized = tf.image.resizeBilinear(tensor, [28, 28]).toFloat()
+        
+        //normalize 
+        const offset = tf.scalar(255.0);
+        const normalized = tf.scalar(1.0).sub(resized.div(offset));
+
+        //add a dimension to get a batch shape 
+        const batched = normalized.expandDims(0)
+        return batched
+    })
+}
+
+/*
+load the model
+*/
+async function start(cur_mode) {
+    
+    mode = cur_mode
+    
+    //load the model 
+    model = await tf.loadModel('model2/model.json')
+    
+    //warm up 
+    model.predict(tf.zeros([1, 28, 28, 1]))
+    
+    //allow drawing on the canvas 
+    allowDrawing()
+    
+    //load the class names
+    await loadDict()
+}
+
+/*
+allow drawing on canvas
+*/
+function allowDrawing() {
+    canvas.isDrawingMode = 1;
+    document.getElementById('status').innerHTML = 'Start Drawing!';
+    $('button').prop('disabled', false);
+    var slider = document.getElementById('myRange');
+    slider.oninput = function() {
+        canvas.freeDrawingBrush.width = this.value;
+    };
+}
+
+/*
+submit keywords to main.html
+*/
+function submitkeys(){
+	window.location.href="test.html?keywords='"+a[0]+" "+a[1]+" "+a[2]+"'";
+}
+
+/*
+clear the canvs 
+*/
+function erase() {
+    canvas.clear();
+    canvas.backgroundColor = '#ffffff';
+    coords = [];
+}
