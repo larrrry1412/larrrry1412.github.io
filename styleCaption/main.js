b0ce32068807cd4e82222fa8a0ebdc404fd539d9
sketchers recognition <1244304379@qq.com>
2019-03-17 15:49:27 +0800

    Update main.js

--- styleCaption/main.js
+++ styleCaption/main.js
@@ -1,100 +1,13 @@
 /*
 variables
 */
-var model;
-var canvas;
-var classNames = [];
-var canvas;
-var coords = [];
-var mousePressed = false;
-var mode;
+var modelIncep;
+var modelstyle;
 var a=[];
+var idx2word=[];
+var word2idx = new Array();
 
-/*
-prepare the drawing canvas 
-*/
-$(function() {
-    canvas = window._canvas = new fabric.Canvas('canvas');
-    canvas.backgroundColor = '#ffffff';
-    canvas.isDrawingMode = 0;
-    canvas.freeDrawingBrush.color = "black";
-    canvas.freeDrawingBrush.width = 10;
-    canvas.renderAll();
-    //recognize once mouse_up
-    canvas.on('mouse:up', function(e) {
-        getFrame();
-        mousePressed = false
-    });
-    canvas.on('mouse:down', function(e) {
-        mousePressed = true
-    });
-    canvas.on('mouse:move', function(e) {
-        recordCoor(e)
-    });
-})
 
-/*
-set the table of the predictions 
-*/
-function setTable(top5, probs) {
-    //loop over the predictions 
-    for (var i = 0; i < top5.length; i++) {
-        let sym = document.getElementById('sym' + (i + 1))
-        let prob = document.getElementById('prob' + (i + 1))
-	let b = document.getElementById('b'+ (i + 1))
-        sym.innerHTML = top5[i]
-	a[i]=top5[i]
-	b.innerHTML=top5[i]
-        prob.innerHTML = Math.round(probs[i] * 100)
-    }
-    //create the pie 
-    createPie(".pieID.legend", ".pieID.pie");
-
-}
-
-/*
-record the current drawing coordinates
-*/
-function recordCoor(event) {
-    var pointer = canvas.getPointer(event.e);
-    var posX = pointer.x;
-    var posY = pointer.y;
-
-    if (posX >= 0 && posY >= 0 && mousePressed) {
-        coords.push(pointer)
-    }
-}
-
-/*
-cause the image data input should be (28,28,1)
-so get the best bounding box by trimming around the drawing
-will benefit the result
-*/
-function getMinBox() {
-    //get coordinates 
-    var coorX = coords.map(function(p) {
-        return p.x
-    });
-    var coorY = coords.map(function(p) {
-        return p.y
-    });
-
-    //find top left and bottom right corners 
-    var min_coords = {
-        x: Math.min.apply(null, coorX),
-        y: Math.min.apply(null, coorY)
-    }
-    var max_coords = {
-        x: Math.max.apply(null, coorX),
-        y: Math.max.apply(null, coorY)
-    }
-
-    //return box cordinates
-    return {
-        min: min_coords,
-        max: max_coords
-    }
-}
 
 /*
 get the image data 
@@ -204,12 +117,9 @@ function preprocess(imgData) {
         //convert to a tensor 
         let tensor = tf.fromPixels(imgData, numChannels = 1)
         
-        //resize 
-        const resized = tf.image.resizeBilinear(tensor, [28, 28]).toFloat()
-        
         //normalize 
         const offset = tf.scalar(255.0);
-        const normalized = tf.scalar(1.0).sub(resized.div(offset));
+        const normalized = tf.scalar(1.0).sub(tensor.div(offset));
 
         //add a dimension to get a batch shape 
         const batched = normalized.expandDims(0)
@@ -220,102 +130,36 @@ function preprocess(imgData) {
 /*
 load the model
 */
-async function start(cur_mode) {
-    
-    mode = cur_mode
+async function start() {
     
     //load the model 
-    model = await tf.loadModel('model2/model.json')
+    modelIncep = await tf.loadModel('modelnew/model.json')
+	modelstyle = await tf.loadModel('modelstyle/model.json')
     
-    //warm up 
-    model.predict(tf.zeros([1, 28, 28, 1]))
     
-    //allow drawing on the canvas 
-    allowDrawing()
-    
-    //load the class names
+    //load the wordsdict
     await loadDict()
 }
 
-/*
-allow drawing on canvas
-*/
-function allowDrawing() {
-    canvas.isDrawingMode = 1;
-    document.getElementById('status').innerHTML = '开始绘画吧!';
-    $('button').prop('disabled', false);
-    var slider = document.getElementById('myRange');
-    slider.oninput = function() {
-        canvas.freeDrawingBrush.width = this.value;
-    };
-}
-
-/*
-submit keywords to main.html
-*/
-function submitkey1(){
-	window.location.href="http://localhost:8080/#/result?keyword="+a[0];
-}
-function submitkey2(){
-	window.location.href="http://localhost:8080/#/result?keyword="+a[1];
-}
-function submitkey3(){
-	window.location.href="http://localhost:8080/#/result?keyword="+a[2];
-}
-function submitkey4(){
-	window.location.href="http://localhost:8080/#/result?keyword="+a[3];
-}
-function submitkey5(){
-	window.location.href="http://localhost:8080/#/result?keyword="+a[4];
-}
-/*
-clear the canvs 
-*/
-function erase() {
-    canvas.clear();
-    canvas.backgroundColor = '#ffffff';
-    coords = [];
-}
 
 function uploadImg(file,imgNum){
-   var widthImg = 200; //显示图片的width
-   var heightImg = 200; //显示图片的height
+   var widthImg = 299; //显示图片的width
+   var heightImg = 299; //显示图片的height
    var div = document.getElementById(imgNum);
    if (file.files && file.files[0]){
         div.innerHTML ='<img id="upImg">'; //生成图片
-        var img = document.getElementById('upImg'); //获得用户上传的图片节点
+        img = document.getElementById('upImg'); //获得用户上传的图片节点
         img.onload = function(){
-        img.width = widthImg;
-        img.height = heightImg;
-    }
-var reader = new FileReader(); //判断图片是否加载完毕	
-reader.onload = function(evt){
-if(reader.readyState === 2){ //加载完毕后赋值
-img.src = evt.target.result;
-}
-}
-reader.readAsDataURL(file.files[0]); 
-}
-}
+			img.width = widthImg;
+			img.height = heightImg;
+		}
+		var reader = new FileReader(); //判断图片是否加载完毕	
+		reader.onload = function(evt){
+			if(reader.readyState === 2){ //加载完毕后赋值
+				img.src = evt.target.result;
+			}
+		}
+		reader.readAsDataURL(file.files[0]); 
+	}
 
-
-var image;
-img.onchange = function () {
-    let file = document.querySelector('input[type=file]').files[0]  // 获取选择的文件，这里是图片类型
-    let reader = new FileReader()
-    reader.readAsDataURL(file) //读取文件并将文件以URL的形式保存在resulr属性中 base64格式
-    reader.onload = function(e) { // 文件读取完成时触发 
-        let result = e.target.result // base64格式图片地址 
-        image = new Image();
-        image.src = result // 设置image的地址为base64的地址 
-        image.onload = function(){ 
-            var canvas = document.querySelector("#canvas"); 
-            var context = canvas.getContext("2d"); 
-            canvas.width = image.width; // 设置canvas的画布宽度为图片宽度 
-            canvas.height = image.height; 
-            context.drawImage(image, 0, 0, image.width, image.height) // 在canvas上绘制图片 
-            //let dataUrl = canvas.toDataURL('image/jpeg', 0.92) // 0.92为压缩比，可根据需要设置，设置过小会影响图片质量 
-            // dataUrl 为压缩后的图片资源，可将其上传到服务器 
-        } 
-    }
 }
